8c3530262a735a71f654088dfffb470a79fdca07
shaikhaffan12 <100150277+shaikhaffan12@users.noreply.github.com>
2022-03-30 17:08:52 +0530

    push all codes from 28 to 30 march

---
+++ week 7 assignments python/if_else_prac.py
@@ -0,0 +1,13 @@
+def prog():
+    objone = input("Enter Something: ")
+    objtwo = input("Enter Something: ")
+
+    if objone == objtwo:
+        return True
+
+    else:
+        return False
+
+
+obj = prog()
+print(obj)
