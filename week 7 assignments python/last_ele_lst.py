3bf6d4c424f2322027d89bc1b72c481f7efab178
shaikhaffan12 <100150277+shaikhaffan12@users.noreply.github.com>
2022-04-04 12:50:35 +0530

    Delete week 7 assignments python directory

--- week 7 assignments python/last_ele_lst.py
+++
@@ -1,9 +0,0 @@
-
-def get_last(lst):
-    get_ele = lst.pop()
-
-    print(get_ele)
-
-
-lsti = ['affan', 1,2,3,4,'jack']
-get_last(lsti)
--- week 7 assignments python/recursion.py
+++
@@ -1,8 +0,0 @@
-def repeat(string,a):
-    if a==0:
-        return 1
-    else:
-        print(string)
-        return repeat(string,a-1)
-
-repeat('affan', 5)
